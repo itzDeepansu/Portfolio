456f621e29e486e6e36a172aa6ce176bf3e93940
Deepansu Siwach <itzdeepansu@gmail.com>
2024-10-23 01:26:00 +0530

    Added Vortex to Background

--- app/page.js
+++ app/page.js
@@ -7,9 +7,9 @@ import SkillsSection from "./components/SkillsSection";
 import { useTheme } from "next-themes";
 import ProjectSection from "./components/ProjectSection";
 import Experiences from "./components/Experiences";
+import { Vortex } from "@/components/ui/vortex";
 
 export default function Home() {
-
   const { setTheme } = useTheme();
   setTheme("dark");
 
@@ -63,54 +63,68 @@ export default function Home() {
   return (
     <div className="image-container h-[5000px] relative mx-10 box-border font-jetbr">
       <Navbar classes="sticky top-0" />
-      <section className={`h-[80dvh] grid grid-cols-2 gap-96 ${scrollTopper >=200 ? "animate-fadeOut opacity-0" : "animate-fadeIn"}`}>
-        <div className="h-[80dvh] text-7xl overflow-hidden pt-36 pl-10">
-          <div className="font-dancing text-9xl">Creative</div>
-          <TypingText
-            text="Developer."
-            waitTime="1200"
-            hideCursorOnComplete={true}
-            delay={60}
-          />
-        </div>
-        <div className="h-[80dvh] flex flex-col justify-center gap-6 text-sm pl-16">
-          <ul className="list-none">
-            <li className="text-mutedForeground">
-              Hi! I'm Deepansu Siwach , an India based
-            </li>
-            <li>
-              creative web developer
-              <span className="text-mutedForeground"> and </span>
-              web designer
-            </li>
-          </ul>
-          <ul className="list-none text-mutedForeground">
-            <li>I started learning web development about an year ago</li>
-            <li>
-              but I also have interest in
-              <span className="text-foreground"> Artificial Intelligence </span>
-            </li>
-            <li>
-              and
-              <span className="text-foreground"> Machine Learning </span>. I
-              want to use my skills of
-            </li>
-            <li>
-              Web and AI to solve{" "}
-              <TypingText
-                text=" real world problems"
-                waitTime="1200"
-                hideCursorOnComplete={true}
-                delay={60}
-                className="text-foreground inline-flex"
-                grow
-              />
-              .
-            </li>
-          </ul>
-        </div>
-      </section>
 
+      <section
+        className={` ${
+          scrollTopper >= 200 ? "animate-fadeOut opacity-0" : "animate-fadeIn"
+        }`}
+      >
+        <Vortex
+          backgroundColor="black"
+          baseHue={100}
+          particleCount={500}
+          className="h-[80dvh] grid grid-cols-2 gap-96"
+        >
+          <div className="h-[80dvh] text-7xl overflow-hidden pt-36 pl-10">
+            <div className="font-dancing text-9xl">Creative</div>
+            <TypingText
+              text="Developer."
+              waitTime="1200"
+              hideCursorOnComplete={true}
+              delay={60}
+            />
+          </div>
+          <div className="h-[80dvh] flex flex-col justify-center gap-6 text-sm pl-16">
+            <ul className="list-none">
+              <li className="text-mutedForeground">
+                Hi! I'm Deepansu Siwach , an India based
+              </li>
+              <li>
+                creative web developer
+                <span className="text-mutedForeground"> and </span>
+                web designer
+              </li>
+            </ul>
+            <ul className="list-none text-mutedForeground">
+              <li>I started learning web development about an year ago</li>
+              <li>
+                but I also have interest in
+                <span className="text-foreground">
+                  {" "}
+                  Artificial Intelligence{" "}
+                </span>
+              </li>
+              <li>
+                and
+                <span className="text-foreground"> Machine Learning </span>. I
+                want to use my skills of
+              </li>
+              <li>
+                Web and AI to solve{" "}
+                <TypingText
+                  text=" real world problems"
+                  waitTime="1200"
+                  hideCursorOnComplete={true}
+                  delay={60}
+                  className="text-foreground inline-flex"
+                  grow
+                />
+                .
+              </li>
+            </ul>
+          </div>
+        </Vortex>
+      </section>
       <SkillsSection
         className={`${scrollTopper >= 260 ? "animate-fadeIn" : "hidden"} ${
           scrollTopper >= 1370 ? "animate-fadeOut opacity-0" : ""
--- app/test/page.jsx
+++ app/test/page.jsx
@@ -3,7 +3,15 @@
 import React, { useRef } from "react";
 import { AnimatedBeam } from "@/components/ui/animated-beam";
 // Assuming AnimatedBeam is a component you still need.
-import { ExpressIcon, MongodbIcon, PostgresqlIcon, ReactIcon, SupabaseIcon, WWWIcon } from "../components/icons/icon";
+import {
+  ExpressIcon,
+  MongodbIcon,
+  PostgresqlIcon,
+  ReactIcon,
+  SupabaseIcon,
+  WWWIcon,
+} from "../components/icons/icon";
+import { Vortex } from "@/components/ui/vortex";
 
 export default function AnimatedBeamDemoPage() {
   const containerRef = useRef(null);
@@ -15,83 +23,91 @@ export default function AnimatedBeamDemoPage() {
   const div6Ref = useRef(null);
 
   return (
-    <div
-      className="relative flex w-full max-w-[500px] items-center justify-center overflow-hidden rounded-lg border bg-background p-10"
-      ref={containerRef}
-    >
-      <div
-        ref={div1Ref}
-        className="flex items-center justify-center w-12 h-12 rounded-full bg-white shadow-md z-10"
+    <div className="bg-red-900 h-screen w-screen">
+      <Vortex
+        backgroundColor="black"
+        particleCount={500}
+        className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
       >
-        <WWWIcon />
-      </div>
-      <div className="flex size-full max-w-lg flex-col items-center justify-between">
         <div
-          ref={div2Ref}
-          className="flex items-center justify-center w-12 h-12 shadow-md z-10"
+          className="relative flex w-full max-w-[500px] items-center justify-center overflow-hidden rounded-lg border bg-background p-10"
+          ref={containerRef}
         >
-          <ReactIcon />
+          <div
+            ref={div1Ref}
+            className="flex items-center justify-center w-12 h-12 rounded-full bg-white shadow-md z-10"
+          >
+            <WWWIcon />
+          </div>
+          <div className="flex size-full max-w-lg flex-col items-center justify-between">
+            <div
+              ref={div2Ref}
+              className="flex items-center justify-center w-12 h-12 shadow-md z-10"
+            >
+              <ReactIcon />
+            </div>
+            <div
+              ref={div3Ref}
+              className="flex items-center justify-center w-12 h-12 shadow-md z-10"
+            >
+              <ExpressIcon />
+            </div>
+            <div
+              ref={div4Ref}
+              className="flex items-center justify-center w-12 h-12 shadow-md z-10"
+            >
+              <MongodbIcon />
+            </div>
+            <div
+              ref={div5Ref}
+              className="flex items-center justify-center w-12 h-12 shadow-md z-10"
+            >
+              <PostgresqlIcon />
+            </div>
+            <div
+              ref={div6Ref}
+              className="flex items-center justify-center w-12 h-12 shadow-md z-10"
+            >
+              <SupabaseIcon />
+            </div>
+          </div>
+          <AnimatedBeam
+            duration={3}
+            containerRef={containerRef}
+            fromRef={div1Ref}
+            toRef={div2Ref}
+            curvature={100}
+          />
+          <AnimatedBeam
+            duration={3}
+            containerRef={containerRef}
+            fromRef={div1Ref}
+            toRef={div3Ref}
+            curvature={40}
+          />
+          <AnimatedBeam
+            duration={3}
+            containerRef={containerRef}
+            fromRef={div1Ref}
+            toRef={div4Ref}
+            curvature={0}
+          />
+          <AnimatedBeam
+            duration={3}
+            containerRef={containerRef}
+            fromRef={div1Ref}
+            toRef={div5Ref}
+            curvature={-40}
+          />
+          <AnimatedBeam
+            duration={3}
+            containerRef={containerRef}
+            fromRef={div1Ref}
+            toRef={div6Ref}
+            curvature={-100}
+          />
         </div>
-        <div
-          ref={div3Ref}
-          className="flex items-center justify-center w-12 h-12 shadow-md z-10"
-        >
-          <ExpressIcon />
-        </div>
-        <div
-          ref={div4Ref}
-          className="flex items-center justify-center w-12 h-12 shadow-md z-10"
-        >
-          <MongodbIcon />
-        </div>
-        <div
-          ref={div5Ref}
-          className="flex items-center justify-center w-12 h-12 shadow-md z-10"
-        >
-          <PostgresqlIcon />
-        </div>
-        <div
-          ref={div6Ref}
-          className="flex items-center justify-center w-12 h-12 shadow-md z-10"
-        >
-          <SupabaseIcon />
-        </div>
-      </div>
-      <AnimatedBeam
-        duration={3}
-        containerRef={containerRef}
-        fromRef={div1Ref}
-        toRef={div2Ref}
-        curvature={100}
-      />
-      <AnimatedBeam
-        duration={3}
-        containerRef={containerRef}
-        fromRef={div1Ref}
-        toRef={div3Ref}
-        curvature={40}
-      />
-      <AnimatedBeam
-        duration={3}
-        containerRef={containerRef}
-        fromRef={div1Ref}
-        toRef={div4Ref}
-        curvature={0}
-      />
-      <AnimatedBeam
-        duration={3}
-        containerRef={containerRef}
-        fromRef={div1Ref}
-        toRef={div5Ref}
-        curvature={-40}
-      />
-      <AnimatedBeam
-        duration={3}
-        containerRef={containerRef}
-        fromRef={div1Ref}
-        toRef={div6Ref}
-        curvature={-100}
-      />
+      </Vortex>
     </div>
   );
 }
